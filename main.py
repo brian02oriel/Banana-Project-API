4b844456df486ee31bd24d1f8805fe2ca4964eae
brian02oriel <brian.nieto1@utp.ac.pa>
2021-03-28 23:16:06 -0500

    Initial structure

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import flask
+from flask import request, jsonify
+from flask_cors import CORS, cross_origin
+
+app = flask.Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "Welcome to Banana Project API"
+
