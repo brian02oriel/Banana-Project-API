cdd6afd793e676e76d4c8bc4e0be0035348238a5
Brian Nieto <brian.nieto1@utp.ac.pa>
2021-04-03 23:01:36 -0500

    test

--- Classifier/image_features.py
+++ Classifier/image_features.py
@@ -22,12 +22,12 @@ def Decode_Extract_Features(base64_img):
     net = cv2.dnn.readNetFromCaffe(deploy_prototxt, model_file)
     features = get_features(img, net)
     print(features.shape)
-    features = features.reshape(-1)
+    features = features.reshape(-1, 1)
     print(features.shape)
     #kmeans = load("Classifier/Models/KMEANS_MODEL.joblib")
     with open("Classifier/Models/KMEANS_MODEL.pkl", "rb") as file:
       kmeans = pickle.load(file)
-    kmeans_prediction = kmeans.predict([features])
+    kmeans_prediction = kmeans.predict(features)
     kmeans_centers = kmeans.cluster_centers_
     prediction_center = kmeans_centers[kmeans_prediction]
     kmeans_distance_from_center = euclidean_distances([features], [prediction_center])
