cf12be3995026a3736947543d746e1a6d38daa3e
Brian Nieto <brian.nieto1@utp.ac.pa>
2021-04-03 23:10:51 -0500

    test

--- Classifier/image_features.py
+++ Classifier/image_features.py
@@ -22,7 +22,7 @@ def Decode_Extract_Features(base64_img):
     net = cv2.dnn.readNetFromCaffe(deploy_prototxt, model_file)
     features = get_features(img, net)
     print(features.shape)
-    features = np.ravel(features)
+    features = features.reshape(-1, 1)
     print(features.shape)
     #kmeans = load("Classifier/Models/KMEANS_MODEL.joblib")
     with open("Classifier/Models/KMEANS_MODEL.pkl", "rb") as file:
